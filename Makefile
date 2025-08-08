a8d4cab6034e39ef55e5f6803cbfb7f9b7450a3f
Dave <kinneyd81@gmail.com>
2025-08-07 18:34:52 -0700

    first commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+TARGET = bin/dbview
+SRC = $(wildcard src/*.c)
+OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
+
+run: clean default
+	./$(TARGET) -h
+	./$(TARGET) -n -f ./mynewdb.db 
+	./$(TARGET) -f ./mynewdb.db -a "Timmy H.,123 Sheshire Ln.,120"
+	./$(TARGET) -f ./mynewdb.db -l
+
+default: $(TARGET)
+
+clean:
+	rm -f obj/*.o
+	rm -f bin/*
+	rm -f *.db
+
+$(TARGET): $(OBJ)
+	gcc -o $@ $?
+
+obj/%.o : src/%.c
+	gcc -c $< -o $@ -Iinclude
+
+
---
+++ src/main.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "file.h"
+#include "parse.h"
+
+//void print_usage(char *argv[]) { <-- orginally this?
+void print_usage() {
+    printf("Usage: -n -f <file>\n");
+    printf("\t -f  - file to create.\n");
+    printf("\t -h  - show help menu.\n");
+    printf("\t -n  - create new database.\n");
+    return;
+}
+
+int main(int argc, char *argv[]) { 
+    char *filepath = NULL;
+    char *addstring = NULL;
+    bool newfile = false ;
+    bool list = false ;
+    int dbfd = -1;
+    int c;
+    struct dbheader_t *dbhdr = NULL;
+    struct employee_t *employees = NULL;
+
+    while ((c = getopt(argc, argv, "a:hlnf:")) != -1) {
+        switch (c) {
+            case 'a':
+                    addstring = optarg;
+                    break;
+            case 'n':
+                    newfile = true;
+                    break;
+            case 'f':
+                    filepath = optarg;
+                    break;
+            case 'l':
+                    list = true;
+                    break;
+            case 'h':
+                    print_usage();
+                    return 0;
+            default : /* '?' */
+                    print_usage();
+                    return -1;
+        }
+    }	
+
+    if (filepath == NULL) {
+        printf("Filepath is a required argument.\n");
+        print_usage(&argv[0]);
+        return 0;
+    }
+
+    // If flag set, creat new file
+    if (newfile) {
+        dbfd = create_db_file(filepath); // returns database filedescriptor
+
+        if (dbfd == -1) {
+            printf("Unable to create db file, %s\n");
+            return STATUS_ERROR;
+        }
+
+        if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR ) {
+            printf("Failed to create db header\n");
+            return -1;
+        }
+        output_file(dbfd, dbhdr, employees);
+
+    } else {
+        dbfd = open_db_file(filepath);
+        if (dbfd == STATUS_ERROR) {
+             printf("Unable to open db file, %s\n", filepath);
+             return -1;
+        }
+        if (validate_db_header(dbfd, &dbhdr) == -1) {
+            printf("Bad db header.\n");
+            return -1;
+        }
+    }
+
+    if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS) {
+        printf("Failed to read employees");
+        return 0;
+    }
+
+    if (addstring) {
+        dbhdr->count++;
+        employees = realloc(employees, dbhdr->count*(sizeof(struct employee_t)));
+        add_employee(dbhdr, employees, addstring);
+        output_file(dbfd, dbhdr, employees);
+    }
+
+    if (list) {
+        list_employees(dbhdr, employees);
+    }
+
+    printf("Newfile: %d\n", newfile);
+    printf("Filepath: %s\n", filepath);
+    return 0;
+}
