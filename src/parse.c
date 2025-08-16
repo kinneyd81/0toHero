a328a1980975df4f4136e82a5bb064fb510f2e86
Dave <kinneyd81@gmail.com>
2025-08-16 08:53:34 -0700

    WIP: fix create_db_header

--- src/parse.c
+++ src/parse.c
@@ -97,7 +97,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
     }
     // Get space on heap for our header
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header == NULL) {
+    if (header == -1) {
         printf("Malloc failed to create db header.\n");
         return STATUS_ERROR;
     }
@@ -141,7 +141,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 int create_db_header(int fd, struct dbheader_t **headerOut) {
 
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header == NULL) {
+    if (header == -1) {
         printf("Malloc failed to create db header\n");
         return STATUS_ERROR;
     }
