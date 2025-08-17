c3934feb537543d69c588defaadef0d2c7e39e82
Dave <kinneyd81@gmail.com>
2025-08-17 11:52:19 -0700

    Employee error checking

--- src/main.c
+++ src/main.c
@@ -95,6 +95,10 @@ int main(int argc, char *argv[]) {
     if (addstring) {
         dbhdr->count++;
         employees = realloc(employees, dbhdr->count*(sizeof(struct employee_t)));
+        if (employees == NULL) {
+            printf("ERROR NULL employee.\n");
+            return STATUS_ERROR;
+        }
         if (add_employee(dbhdr, employees, addstring)) {
             printf("Error adding employee.\n");
             return STATUS_ERROR;
